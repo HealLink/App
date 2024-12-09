0a8344dea101dff6a6588bfff282650da12794bf
“RegaRohadi” <regarohadi422@gmail.com>
2024-12-09 15:12:46 +0700

    Fix error

--- app/src/main/java/com/rega/heallink/ui/auth/LoginViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.rega.heallink.ui.auth
-
-import androidx.lifecycle.ViewModel
-import com.rega.heallink.data.UserRepository
-
-class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
-
-    suspend fun login(email: String, password: String) = userRepository.login(email, password)
-}
